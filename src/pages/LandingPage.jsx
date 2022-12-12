06a39192704793853d14e34eaf4db591d74777d1
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2022-12-12 18:42:40 +0700

    MostPicked done for now

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,12 +5,19 @@ import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
-        <MostPicked data={landingPage.mostPicked} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }
