5f61826c540c9c963e0286ed14ca369152f67dcb
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2022-12-27 15:49:16 +0700

    progress Inputdate

--- src/elements/Form/InputDate/index.jsx
+++ src/elements/Form/InputDate/index.jsx
@@ -7,6 +7,9 @@ import "./index.scss";
 import "react-date-range/dist/style.css"; //main css file
 import "react-date-range/dist/theme/default.css"; //theme css file
 
+import formatDate from "utils/formatDate";
+import iconCalendar from "assets/images/Icons/icon-calendar.svg";
+
 export default function Date(props) {
   const { value, placeholder, name } = props;
   const [isShowed, setIsShowed] = useState(false);
@@ -21,7 +24,46 @@ export default function Date(props) {
     props.onChange(target);
   };
 
-  return <div>index</div>;
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside); // biar kalo click diluar kalender = close kalender
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const refDate = useRef(null);
+  const handleClickOutside = (event) => {
+    if (refDate && refDate.current.contains(event.target)) {
+      setIsShowed(false);
+    }
+  };
+
+  const check = (focus) => {
+    focus.indexOf(1) < 0;
+  };
+
+  const displayDate = `${value.startDate ? formatDate(value.startDate) : ""}${
+    value.endDate ? " - " + formatDate(value.endDate) : ""
+  }`;
+
+  return (
+    <div
+      ref={refDate}
+      className={["input-date mb-3", props.outerClassName].join(" ")}
+    >
+      <div className="input-group">
+        <div className="input-group-prepend bg-gray-900">
+          <div className="input-group-text">
+            <img
+              src={iconCalendar}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Date.propTypes = {
