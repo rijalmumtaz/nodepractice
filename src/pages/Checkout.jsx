a1c069a49b57205219d9591117f595453a6fa732
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-02-18 23:50:07 +0700

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,16 @@
-import Header from "parts/Header";
 import React, { Component } from "react";
+
+import Header from "parts/Header";
+import Button from "elements/Button";
+import Stepper, {
+  Numbering,
+  Controller,
+  Meta,
+  MainContent,
+} from "elements/Stepper";
+import Bookinginformation from "parts/Checkout/Bookinginformation";
+import Payment from "parts/Checkout/Payment";
+import Completed from "parts/Checkout/Completed";
 
 export default class Checkout extends Component {
   render() {
