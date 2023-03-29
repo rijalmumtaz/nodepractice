40d9b6422f928fb738350d6d326766cd598d87d9
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-03-29 17:05:16 +0700

    Fixing Configured Redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "store";
+import configureStore from "store";
+
+const store = configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
-import { configureStore, applyMiddleware, compose } from "@reduxjs/toolkit";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+  legacy_createStore,
+} from "@reduxjs/toolkit";
 // redux-thunk, which allows simple asynchronous use of dispatch
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducer from "./reducers/index";
 
 // init state
 const initialState = {};
@@ -9,16 +14,17 @@ const initialState = {};
 // const for put middleware which have imported
 const middleware = [thunk];
 
-const store = configureStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    // checking redux devtools extension is exist in browser or not
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
-  )
-);
-
-export default store;
+export default function configureStore(initialState) {
+  const store = legacy_createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      // checking redux devtools extension is exist in browser or not
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : (f) => f
+    )
+  );
+  return store;
+}
